f65e06099edec51b93dbc01f8489ab26b32e6379
Kanu Kingsley <suge.kanu@gmail.com>
2023-04-11 11:44:23 +0200

    Challenge two: API Automation

--- api/src/features/2_GET.feature
+++ api/src/features/2_GET.feature
@@ -1,6 +1,12 @@
 Feature: As an API I can retrieve objects
 
-  Scenario: As an API I can retrieve all the posts
+  Scenario: As an API I can retrieve the object
     Given I retrieve the "objects"
     And the response was successful
     Then the response status code is 200
+
+
+   Scenario: As an API I cannot retrieve blue
+    Given I cannot retrieve "blue"
+    And the response was unsuccessful
+    Then the response status code is 404
--- api/src/steps/2_get-steps.ts
+++ api/src/steps/2_get-steps.ts
@@ -28,6 +28,32 @@ Given('I retrieve the {string}', async function(this:ScenarioWorld, endpoint:str
 
     const getBody = await globalAPIResponseVariables.response.json()
     objectId = getBody.id
-    console.log(objectId)
+    //console.log(objectId)
+
+})
+
+Given('I cannot retrieve {string}', async function(this:ScenarioWorld, endpoint:string){
+    const {api:{request},globalAPIResponseVariables}=this
+
+    console.log(`I retrieve the ${endpoint}`)
+
+    const respBody = globalBody;
+   //console.log(respBody.id)
+
+    const resId = respBody.id
+   // console.log(resId)
+
+    endpoint = `${endpoint}/${resId}`
+  // console.log(endpoint)
+
+
+    const response = await request.get("https://api.restful-api.dev/"+endpoint)
+
+
+    globalAPIResponseVariables.response = response
+
+    console.log(await globalAPIResponseVariables.response.json())
+
+ 
 
 })
